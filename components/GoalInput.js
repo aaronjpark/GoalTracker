8d234428d242eb43c2f76bd69b80c342ef7f6bbd
Aaron Park <aaronj.park0304@gmail.com>
2025-01-21 08:02:24 -0600

    Add Modal Styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 import { useState } from "react";
-import { View, TextInput, Button, Modal } from "react-native";
+import { View, TextInput, Button, Modal, Image } from "react-native";
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -17,6 +17,7 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require("../assets/images/goal.png")} />
         <TextInput
           style={styles.textInput}
           placeholder="Type in your goal!"
@@ -43,10 +44,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#caf0f8",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
