96811c2067626d5119e629ae1b7f419fb9d815a1
Aaron Park <aaronj.park0304@gmail.com>
2025-01-21 07:04:48 -0600

    implement deleteGoalItem

---
+++ components/GoalItem.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+import { View, Text, Pressable } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <Pressable onPress={props.onDeleteItem}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5AC8FA",
+  },
+  goalText: {
+    color: "white",
+  },
+});
